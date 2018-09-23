f2bc538ba5805330f072f27296aed73892581787
caueaquino <cauenogueira@alunos.utfpr.edu.br>
2018-09-23 16:25:43 -0300

    fixed css problem + add js

---
+++ Website-Project/js/index.js
@@ -0,0 +1,21 @@
+// Javascript Document
+function login(){
+
+}
+
+function pesLogin(lusuario, lsenha){
+    this.lusuario=lusuario;
+    this.lsenha=lsenha;
+}
+
+function cadastro(){
+    
+}
+
+function pesCadastro(){
+    this.cusuario=cusuario;
+    this.csenha=csenha;
+    this.cnome=cnome;
+    this.cemail=cemail;
+}
+
